5689240792c349f562f8cbda005f7acd2a8c9ab6
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-11 23:09:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,12 +20,12 @@ bughunter0 = Client(
     api_hash = os.environ["API_HASH"]
 )
 
-@bughunter0.on_message(filters.command(["start"]))
-async def start(bot, message):
-   await message.reply_text("ചത്തൊന്ന് അറിയാൻ വന്നതാ ല്ലേ.... !!")
+# @bughunter0.on_message(filters.command(["start"]))
+# async def start(bot, message):
+#   await message.reply_text("ചത്തൊന്ന് അറിയാൻ വന്നതാ ല്ലേ.... !!")
 
 @bughunter0.on_message(filters.command(["tts"]))
-async def tts(bot, message) 
+ async def tts(bot, message) 
    #  try:
          text = str(message.reply_to_message.text)
          language = 'en'
