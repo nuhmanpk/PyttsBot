951ac7e54cab564428bfb79b87b37eec89589a93
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-15 19:52:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,26 +22,25 @@ bughunter0 = Client(
 
 @bughunter0.on_message(filters.command(["start"]))
 async def start(bot, message):
-   await message.reply_text("Sent me the Text, Then Reply /pytts to the message!! \n @BughunterBots")
+   await message.reply_text("Hi, Sent Me a The message for Converting to Speech \n\n @BughunterBots")
 
-@bughunter0.on_message(filters.command(["pytts"]))
+@bughunter0.on_message((filters.text | filters.forwarded | filters.reply) & filters.private)
 async def tts(bot, message):
   try:
-      if message.reply_to_message is None:
-            await message.reply_text("Please use /pytts as a Reply to a Text")
-      else :
-            if message.reply_to_message.text:
-                  text = str(message.reply_to_message.text)
-                # change Language from here
-                  language = 'en-in'  # 'en': ['en-us', 'en-ca', 'en-uk', 'en-gb', 'en-au', 'en-gh', 'en-in',
-                                      # 'en-ie', 'en-nz', 'en-ng', 'en-ph', 'en-za', 'en-tz'],
-                  tts_file = gTTS(text=text, lang=language, slow=False) 
-                  tts_file.save(f"{message.chat.id}.mp3") 
-                  chat_id = str(message.chat.id)
-                  with open(f"{message.chat.id}.mp3", "rb") as speech:
-                          await bot.send_voice(chat_id, speech, caption ="@BugHunterBots")
-            else:
-                  await message.reply_text("Ouch !! I can't find Text in this message")
+       if message.reply_to_message.text:
+          text = str(message.text)
+        # change Language from here
+          language = 'en-in'  # 'en': ['en-us', 'en-ca', 'en-uk', 'en-gb', 'en-au', 'en-gh', 'en-in',
+                              # 'en-ie', 'en-nz', 'en-ng', 'en-ph', 'en-za', 'en-tz'],
+          tts_file = gTTS(text=text, lang=language, slow=False) 
+          tts_file.save(f"{message.chat.id}.mp3") 
+          tx = await bot.send_message("Converting to Voice...")
+          chat_id = str(message.chat.id)
+          with open(f"{message.chat.id}.mp3", "rb") as speech:
+                await bot.send_voice(chat_id, speech, caption ="@BugHunterBots")
+          await tx.delete()
+       else:
+          await message.reply_text("Ouch !! I can't find Text in this message")
   except Exception as error:
        print (error)
        await message.reply_text("Oops Something Bad occurred!!")
