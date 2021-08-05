765a320ee231339910e01eb6f06e101b5f2632f5
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-08-05 12:31:05 +0530

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
+import os
 
 DB_URI = os.environ.get('DATABASE_URL')
 
