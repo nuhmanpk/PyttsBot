a11332b2228090e0f839f1431ceed7ee22854b04
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-08-05 12:33:36 +0530

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
-DB_URI = os.environ.get('DATABASE_URL')
+DB_URI = "postgres://zrxnzwzeiypfzw:75387f4557cd36b6a036864a5927d9e128955fa9e8478fa2af8d9360d13a3941@ec2-18-211-171-122.compute-1.amazonaws.com:5432/d60du9ge6npgc2"
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
