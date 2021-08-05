f84783eef51f6ae3cc7101cc3d46994d8b376fbd
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-08-05 12:08:56 +0530

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,15 @@
-from db.database import Database
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+DB_URI = os.environ.get('DATABASE_URL')
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
